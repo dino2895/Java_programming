90c9fc67751a85b3c117c4b84cede93176591df5
dino2895 <dino2895un@gmail.com>
2021-10-24 00:38:22 +0800

    Unit06_20

---
+++ Unit06_hw/Unit06_20.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Unit06_20 {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		String inputStr,outputStr;
+		int inputInt,outputInt;
+		inputStr=sc.next();
+		inputInt=sc.nextInt();
+        //
+        outputInt = new Integer(inputStr);
+        outputInt +=3;
+        outputStr = Integer.toString(inputInt)+"3";
+        //
+		System.out.println(outputInt);
+		System.out.print(outputStr);
+	}
+}
