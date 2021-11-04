54f1d42e1a87d1878ba35328ff9be2b36dd8f2c2
dino2895 <dino2895un@gmail.com>
2021-11-04 16:25:28 +0800

    Unit07_04

---
+++ Unit07_hw/Unit07_04.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Unit07_04 {
+    public static void main(String args[]) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        System.out.println(str.toUpperCase());
+        System.out.print(str.toLowerCase());
+    }
+
+}
