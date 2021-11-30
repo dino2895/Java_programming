2e2f165a8f7d42783d6b8051513c5920163f169a
dino2895 <dino2895un@gmail.com>
2021-12-01 00:27:50 +0800

    Unit08_04

---
+++ Unit08_practice/Unit08_04.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Unit08_04{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+		int i,j=0,x=0;
+		String c;
+		String d;
+		c = sc.next().toUpperCase();
+		d = sc.next().toUpperCase();
+        for(i=0;i<c.length();i++){
+            if(c.charAt(i)==d.charAt(j)){
+                j++;
+                x++;
+            }
+        }
+        if(x==d.length())
+            System.out.print("Y");
+        else 
+            System.out.print("N");
+    }
+}
