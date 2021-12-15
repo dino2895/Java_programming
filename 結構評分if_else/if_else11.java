f6d3ddd6adc1ff2ccaa8965dbefe01d528d1d1a2
dino2895 <dino2895un@gmail.com>
2021-12-15 14:04:43 +0800

    if_else11

--- 結構評分if_else/if_else11.java
+++ 結構評分if_else/if_else11.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class if_else11 {
     public static void main(String[] args) {
-        
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        if(x>99&&x<1000){
+            System.out.print("Y");
+        }
+        else 
+            System.out.print("N");
     }
 }
